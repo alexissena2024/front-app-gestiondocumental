a5b894caa81241b53d2fc7b1682dbc62c25d3aa9
alexissena2024 <acavanzo@soy.sena.edu.co>
2024-10-08 21:42:07 -0500

    ejercicio de consumo de una API con vue Fetch

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
     <router-view></router-view> <!-- Aquí se cargan las vistas según la ruta -->
+    <CountriesList/>
   </div>
 </template>
 
 <script>
+import CountriesList from './components/CountriesList.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+
+  components:{
+    CountriesList
+
+  }
 };
 </script>
+
+
+
 
 <style scoped>
 /* Agrega tus estilos aquí */
